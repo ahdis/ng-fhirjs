c8cdfc859e960e7aca3373f46031193c876b3f45
Oliver Egger <oliver.egger@ahdis.ch>
2018-01-21 21:16:41 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# ng-fhirjs
+
+ng-fhirjs is an angular module for [fhir.js](https://badge.fury.io/js/fhir.js). It uses HttpClient from @angular/common/http as a http adapter and provides a typescript interface to the fhir.js interface. The fhir-js-http service can be dynamically injected into your components. A testsuite has been added to verify the interface, however the interface covers not yet the complete functionality of the fhir.js implementation. Pull requests appreciated!
+
+## usage
+see app how to use the service
+
+## running unit tests
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## packag the library for using it independently
+The service is packaged into library with the help [ng-packagr](https://github.com/dherges/ng-packagr), see the following [article(https://medium.com/@nikolasleblanc/building-an-angular-4-component-library-with-the-angular-cli-and-ng-packagr-53b2ade0701e) for help.
+
+```
+npm run-script packagr
+cd dist
+npm pack
+```
+new project:
+```
+npm install ../ng-fhirjs/dist/ng-fhirjs-0.0.0.tgz
+```
+
+instead of using `import { FhirJsHttpService, FHIR_HTTP_CONFIG }  './modules/ng-fhirjs/fhir-js-http.service'` 
+use now `import { FhirJsHttpService, FHIR_HTTP_CONFIG } from 'ng-fhirjs';`
+
+
+## development
+
+### linking to local npm modules
+if a fork of fhir.js is used you can link to the fork version in the folling way.
+
+```
+cd /somepath/fhir.js
+npm link
+    /usr/local/lib/node_modules/fhir.js -> /somepath/fhir.js
+cd /someotherpath/ng-fhirjs
+npm link fhir.js
+   /someotherpath/node_modules/fhir.js -> /usr/local/lib/node_modules/fhir.js -> /somepaht/fhir.js
+```
+
+## building
+
+first time: 
+npm install
+
+```
+npm run-script build
+npm run packagr
+```
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,102 @@
+import { Component } from '@angular/core';
+import { FhirJsHttpService } from './modules/ng-fhirjs/fhir-js-http.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = 'ng-fhirjs';
+  clickMessage: string;
+  fhirOutput: string;
+
+  constructor(private fhirHttpService: FhirJsHttpService) {
+  this.clickMessage = '';
+    this.fhirOutput = '';
+  }
+
+  async testConformance() {
+    try {
+
+      console.log('Getting a the patient');
+
+      console.log('Step 1: Calling conformance statement');
+
+      const response: ResponseObj = await this.fhirHttpService.conformance({});
+
+      console.log(JSON.stringify(response));
+      this.fhirOutput += ' success ' + JSON.stringify(response);
+    } catch (error) {
+      console.log('error');
+      console.log(JSON.stringify(error));
+      this.fhirOutput += ' error ' + JSON.stringify(error);
+    }
+  }
+
+
+  async testCreate() {
+    try {
+
+      console.log('Creating a the patient');
+      const entry: Entry = {
+        resource: {
+          resourceType: 'Patient'
+        }
+      };
+
+      entry.debug = true;
+      let response = await this.fhirHttpService.create(entry);
+      console.log(JSON.stringify(response));
+      const createdPatient: IResource = response.data;
+
+      let patientId;
+      let patientVersionId;
+
+      this.fhirOutput = ' success ' + '\n';
+      if (response.headers !== undefined) {
+        this.fhirOutput += response.status + '\n';
+        this.fhirOutput += response.headers.get('location') + '\n';
+        this.fhirOutput += 'id:' + response.data.id + '\n';
+        this.fhirOutput += 'versionId:' + response.data.meta.versionId + '\n';
+        if (response.data.id !== undefined) {
+          patientId = response.data.id;
+          patientVersionId = response.data.meta.versionId;
+
+          const read: ReadObj = { id: patientId, type: 'Patient' };
+          response = await this.fhirHttpService.read(read);
+        }
+        console.log(this.fhirOutput);
+      }
+
+    } catch (error) {
+      console.log('error');
+      console.log(JSON.stringify(error));
+      this.fhirOutput = ' error ' + JSON.stringify(error);
+    }
+
+  }
+
+  async testSearch() {
+    try {
+      const response = await this.fhirHttpService.search(
+        { type: 'Patient', query: { name: { $and: [{ $exact: 'Muster' }, { $exact: 'Felix' }] } } });
+      if (response.headers !== undefined) {
+        console.log(response.status);
+        this.fhirOutput = 'total entries: ' + response.data.total;
+        console.log();
+      }
+    } catch (error) {
+      console.log('error');
+      console.log(JSON.stringify(error));
+      this.fhirOutput = ' error ' + JSON.stringify(error);
+    }
+  }
+
+  onClickMe() {
+    this.testCreate();
+    this.clickMessage = 'testCreate called ';
+  }
+
+}
