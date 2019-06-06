a698463a854b53ad45d6934ee93fc338ca179d55
Oliver Egger <oliver.egger@ahdis.ch>
2019-06-06 21:33:33 +0200

    fhirpath 16 update

--- projects/ng-fhirjs/src/lib/fhirpath.d.ts
+++ projects/ng-fhirjs/src/lib/fhirpath.d.ts
@@ -1,3 +1,8 @@
 declare module 'fhirpath' {
-  export function evaluate(resource: any, path: string): any;
+  export function evaluate(
+    resource: any,
+    path: string,
+    context: any
+  ): (resource: any) => any;
+  export function compile(path: string, context: any): (resource: any) => any;
 }
