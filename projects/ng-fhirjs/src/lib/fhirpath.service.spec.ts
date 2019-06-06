d1335bfd49f8f42633de2bdba9f2614cc9b4c23d
Oliver Egger <oliver.egger@ahdis.ch>
2019-06-06 21:33:33 +0200

    fhirpath 16 update

--- projects/ng-fhirjs/src/lib/fhirpath.service.spec.ts
+++ projects/ng-fhirjs/src/lib/fhirpath.service.spec.ts
@@ -2,6 +2,7 @@ import { getTestBed, TestBed } from '@angular/core/testing';
 
 import { FhirPathService } from './fhirpath.service';
 import { IResource } from './fhir-types';
+import { createCapabilityStatementCernerResource } from './test-data/capability-statement-cerner';
 
 describe('FhirPathService', () => {
   let injector: TestBed;
@@ -50,6 +51,54 @@ describe('FhirPathService', () => {
       expect(
         service.evaluate(resource, 'CapabilityStatement.software.name')[0]
       ).toBe('HAPI FHIR Server');
+    });
+  });
+
+  describe('#oauthuritests', () => {
+    const resource = createCapabilityStatementCernerResource();
+
+    it('oauthUriToken', () => {
+      expect(
+        service.evaluate(
+          resource,
+          "rest.security.extension.where(url='http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris').extension.where(url='token').valueUri"
+        )[0]
+      ).toBe(
+        'https://authorization.sandboxcerner.com/tenants/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/protocols/oauth2/profiles/smart-v1/token'
+      );
+      expect(
+        service.evaluateToString(
+          resource,
+          "rest.security.extension.where(url='http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris').extension.where(url='token').valueUri"
+        )
+      ).toBe(
+        'https://authorization.sandboxcerner.com/tenants/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/protocols/oauth2/profiles/smart-v1/token'
+      );
+      expect(service.getOauthUriToken(resource)).toBe(
+        'https://authorization.sandboxcerner.com/tenants/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/protocols/oauth2/profiles/smart-v1/token'
+      );
+    });
+
+    it('oauthUriAuthorize', () => {
+      expect(
+        service.evaluate(
+          resource,
+          "rest.security.extension.where(url='http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris').extension.where(url='authorize').valueUri"
+        )[0]
+      ).toBe(
+        'https://authorization.sandboxcerner.com/tenants/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/protocols/oauth2/profiles/smart-v1/personas/provider/authorize'
+      );
+      expect(
+        service.evaluateToString(
+          resource,
+          "rest.security.extension.where(url='http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris').extension.where(url='authorize').valueUri"
+        )
+      ).toBe(
+        'https://authorization.sandboxcerner.com/tenants/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/protocols/oauth2/profiles/smart-v1/personas/provider/authorize'
+      );
+      expect(service.getOauthUriAuthorize(resource)).toBe(
+        'https://authorization.sandboxcerner.com/tenants/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/protocols/oauth2/profiles/smart-v1/personas/provider/authorize'
+      );
     });
   });
 });
--- projects/ng-fhirjs/src/lib/fhirpath.service.ts
+++ projects/ng-fhirjs/src/lib/fhirpath.service.ts
@@ -6,6 +6,32 @@ import { evaluate } from 'fhirpath';
 })
 export class FhirPathService {
   evaluate(fhir: any, fhirPath: string): any {
-    return evaluate(fhir, fhirPath);
+    return evaluate(fhir, fhirPath, null);
+  }
+
+  evaluateToString(fhir: any, fhirPath: string): string {
+    let result = this.evaluate(fhir, fhirPath);
+    if (
+      result &&
+      result instanceof Array &&
+      (result as Array<string>).length == 1
+    ) {
+      return (result as Array<string>)[0];
+    }
+    return null;
+  }
+
+  getOauthUriToken(capabilityStatement: any): string {
+    return this.evaluateToString(
+      capabilityStatement,
+      "rest.security.extension.where(url='http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris').extension.where(url='token').valueUri"
+    );
+  }
+
+  getOauthUriAuthorize(capabilityStatement: any): string {
+    return this.evaluateToString(
+      capabilityStatement,
+      "rest.security.extension.where(url='http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris').extension.where(url='authorize').valueUri"
+    );
   }
 }
